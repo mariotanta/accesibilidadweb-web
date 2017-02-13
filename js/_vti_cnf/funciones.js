3c7c87b42b6bb202d3723c669fe0cf911d434170
David Abad Quijada <david.abad@tantacom.com>
2017-02-13 12:29:10 +0100

    Desaparece todo el codigo anterior en esta rama

--- js/_vti_cnf/funciones.js
+++
@@ -1,6 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|16 Mar 2005 18:51:57 -0000
-vti_extenderversion:SR|4.0.2.7802
-vti_cacheddtm:TX|16 Mar 2005 18:51:57 -0000
-vti_filesize:IR|397
-vti_backlinkinfo:VX|recursos.html tanta_comunicacion_en.html tantacomunicacion.html tanta_comunicacion.html
